6376d0e1252120ff0b7058f307975dd9af1e7142
Jon Ludlam <jon@recoil.org>
2019-02-06 17:22:57 +0000

    Add sexp_of for Lang
    Signed-off-by: Jon Ludlam <jon@recoil.org>

--- test/print/print.mli
+++ test/print/print.mli
@@ -1,2 +1,8 @@
+module Lang : sig
+
+  val sexp_of_compilation_unit_t : Model.Lang.Compilation_unit.t -> Sexplib.Sexp.t 
+end
+
 val parser_output :
   Format.formatter -> Model.Comment.docs Model.Error.with_warnings -> unit
+
