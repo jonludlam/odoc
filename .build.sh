281f20ff3c71479ad3c370a7c19b2a08dad9ba83
Jon Ludlam <jon@recoil.org>
2019-03-13 16:22:00 +0000

    Updates
    Signed-off-by: Jon Ludlam <jon@recoil.org>

--- .build.sh
+++ .build.sh
@@ -28,4 +28,15 @@ git add .
 git commit -m "Automatic update"
 git show
 
+if [ "${SSH_KEY}" = "" ]; then
+  echo no ssh key secret defined, so not trying to push
+else
+  echo "${SSH_KEY}" > ~/.ssh/odoc-test-output
+  chmod 600 ~/.ssh/odoc-test-output
+  eval `ssh-agent -s`
+  ssh-add -D && ssh-add ~/.ssh/odoc-test-output
+  ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+  git remote set-url origin git@github.com:jonludlam/odoc-test-output
+  git push origin master
+
 
