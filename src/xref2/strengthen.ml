8e39da7ad2702ba6431251f29f1d09d6908036d7
Jon Ludlam <jon@recoil.org>
2019-06-05 17:30:30 +0100

    Add in future suggestion and todos
    Signed-off-by: Jon Ludlam <jon@recoil.org>

--- src/xref2/strengthen.ml
+++ src/xref2/strengthen.ml
@@ -1,3 +1,20 @@
+(* Note for future improvement (suggested by @lpw25):
+
+You can actually do something even more interesting with that case for strengthening.
+
+If you have:
+[module type S = sig type t end]
+
+and you want to strengthen [S] with the module path [M] then you can produce:
+[S with type t = M.t]
+
+The compiler doesn't do this because it doesn't actually have a representation for `with` in its type algebra
+(`with` is always just expanded away right after paprsing). But since we do have a representation for it, 
+this is probably the best thing to produce in this case.
+
+*)
+
+
 let rec signature (prefix : Model.Paths.Path.Resolved.Module.t) sg =
     let open Component.Signature in
     let open Model.Names in
@@ -19,7 +36,13 @@ and module_ (prefix : Model.Paths.Path.Resolved.Module.t) m =
 and module_type (prefix: Model.Paths.Path.Resolved.ModuleType.t) m =
     match m with
     | None -> Some (Component.ModuleType.Path (`Global ((`Resolved (prefix :> Model.Paths.Path.Resolved.t)))))
-    | _ -> m
+    | Some (Component.ModuleType.Path _p) ->
+        (* TODO *)
+        m
+    | Some (Component.ModuleType.With (_,_)) ->
+        (* TODO *)
+        m
+    | Some (Component.ModuleType.Signature _) -> m
 
 and type_ (path: Model.Paths.Path.Resolved.Type.t) t =
     match t with
