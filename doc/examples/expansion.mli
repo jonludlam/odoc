7aa60e897c68fe94ecd32b84d3a9b86c485b773f
Jon Ludlam <jon@recoil.org>
2021-09-08 22:17:27 +0100

    Updates

--- doc/examples/expansion.mli
+++ doc/examples/expansion.mli
@@ -1,8 +1,16 @@
+(** Examples of different features of Expansion *)
+
+(** For details on what each of the following examples is showing,
+see the explanations in the {{!page-features}Features page} *)
+
 module Simple : sig
+    (** Demonstrates simple expansion with a type equality *)
+
     module StringSet : Stdlib.Set.S with type t = string
 end
 
 module Aliases : sig
+    (** Demonstrates expansion when a module is an alias to a hidden module. *)
 
     module Hidden__module : sig
         type t
@@ -13,6 +21,8 @@ module Aliases : sig
 end
 
 module DeepConstraint : sig
+    (** Demonstrates expansion involving a constraint on a submodule *)
+
     module type SIG = sig
         type t
     end
@@ -20,6 +30,19 @@ module DeepConstraint : sig
     module type MODTYPE = sig
         module X : SIG
         module Y : SIG
+    end
+    
+    type foo
+    
+    module M : MODTYPE with type X.t = foo
+end
+
+module DeepConstraint2 : sig
+    (** Demonstrates expansion involving a constraint on a submodule, but the submodule is already a simple signature *)
+
+    module type MODTYPE = sig
+        module X : sig type t end
+        module Y : sig type t end
     end
     
     type foo
@@ -39,4 +62,38 @@ module TypeSubstitution : sig
     end
     
     module X : SIG with type M.t = string
+end
+
+module ModuleTypeOf : sig
+    (** Demonstrates expanding after recovering the type of a module *)
+
+    module A : sig
+
+        (** This comment for [type t] is written in module [A] *)
+        type t
+      
+    end
+      
+    module M : module type of A
+    module M' : module type of struct include A end
+end
+
+module ModuleTypeOfComplications : sig
+    (** Demonstrates the interaction of [module type of] and destructive module substitution *)
+
+    module type S = sig
+        module X : sig
+            type t
+        end
+
+        module type Y = module type of X
+        module type Z = module type of struct include X end
+    end
+
+    module X1 : sig
+        type t
+        type u
+    end
+    
+    module type T = S with module X := X1
 end
