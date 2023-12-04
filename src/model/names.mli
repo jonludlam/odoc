a28e898d4ec318356094faddf136dc25f8ba9a91
Jon Ludlam <jon@recoil.org>
2023-12-04 23:23:37 +0000

    More expressive names

--- src/model/names.mli
+++ src/model/names.mli
@@ -18,35 +18,33 @@ val parenthesise : string -> string
     Note that it is tricky currently to remove references to internal names,
     and hence the 'safe' [to_string] will not currently raise an exception. When
     the model is updated to handle this the exception will be reinstated. *)
-module type Name = sig
-  type t
-
-  val to_string : t -> string
-
-  val to_string_unsafe : t -> string
-  (** [to_string_unsafe] will allow even internal names to be turned into
-        strings. Use with caution. *)
-
-  val make_std : string -> t
-
-  val of_ident : Ident.t -> t
-
-  val internal_of_string : string -> t
-
-  val internal_of_ident : Ident.t -> t
-
-  val is_internal : t -> bool
-
-  val equal : t -> t -> bool
-
-  val compare : t -> t -> int
-
-  val fmt : Format.formatter -> t -> unit
-
-  val is_hidden : t -> bool
-  (** Hidden names are those that contain a double underscore, e.g.
-        [Hidden__module] *)
-end
+    module type Name = sig
+      type t
+    
+      val to_string : t -> string
+    
+      val to_string_unsafe : t -> string
+    
+      val make_std : string -> t
+    
+      val of_ident : Ident.t -> t
+    
+      val hidden_of_string : string -> t
+    
+      val hidden_of_ident : Ident.t -> t
+    
+      val shadowed_of_string : string -> t
+      
+      val shadowed_of_ident : Ident.t -> t
+        
+      val equal : t -> t -> bool
+    
+      val compare : t -> t -> int
+    
+      val fmt : Format.formatter -> t -> unit
+    
+      val is_hidden : t -> bool
+    end
 
 (** Some named objects can't have internal names, so they have this simpler
     module. *)
