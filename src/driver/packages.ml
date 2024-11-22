0baf225baff9c721cec7e69786c30c246ba3ab4b
Jon Ludlam <jon@recoil.org>
2024-11-22 15:54:26 +0000

    Put voodoo output under a doc directory

--- src/driver/packages.ml
+++ src/driver/packages.ml
@@ -91,6 +91,7 @@ type t = {
   remaps : (string * string) list;
   other_docs : Fpath.t list;
   pkg_dir : Fpath.t;
+  doc_dir : Fpath.t;
   config : Global_config.t;
 }
 
@@ -405,6 +406,7 @@ let of_libs ~packages_dir libs =
                         remaps = [];
                         other_docs = [];
                         pkg_dir;
+                        doc_dir=pkg_dir;
                         config;
                       })
               acc)
@@ -486,6 +488,7 @@ let of_packages ~packages_dir packages =
             remaps;
             other_docs = [];
             pkg_dir;
+            doc_dir = pkg_dir;
             config;
           }
           acc)
