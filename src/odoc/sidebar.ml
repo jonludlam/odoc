a6a76bdd99a4539da3069251f35e209251045779
Paul-Elliot <peada@free.fr>
2024-11-29 14:50:10 +0100

    Breadcrumbs: use sidebar index if possible

--- src/odoc/sidebar.ml
+++ src/odoc/sidebar.ml
@@ -1,16 +1,17 @@
 open Or_error
 open Odoc_utils
 
-let toc_to_json ({ url; content = inline; _ } : Odoc_document.Sidebar.entry) :
+let toc_to_json
+    ({ url; valid_link; content = inline; _ } : Odoc_document.Sidebar.entry) :
     Json.json =
   let config =
     Odoc_html.Config.v ~semantic_uris:true ~indent:true ~flat:false
       ~open_details:false ~as_json:true ~remap:[] ()
   in
   let url, kind =
-    match url with
-    | None -> (`Null, `Null)
-    | Some url ->
+    match valid_link with
+    | false -> (`Null, `Null)
+    | true ->
         let href =
           Odoc_html.Link.href ~config ~resolve:(Odoc_html.Link.Base "") url
         in
