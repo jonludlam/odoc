bc52b8f904032db34cc00574bfe10faad525a835
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-03-07 13:37:02 +0100

    Enable odoc_markdown in the cli

--- src/odoc/bin/main.ml
+++ src/odoc/bin/main.ml
@@ -1312,6 +1312,19 @@ end
 
 module Odoc_html = Make_renderer (Odoc_html_args)
 
+module Odoc_markdown_cmd = Make_renderer (struct
+  type args = Odoc_markdown.Config.t
+
+  let render config sidebar page =
+    Odoc_markdown.Generator.render ~config ~sidebar page
+
+  let filepath _url = failwith "Not implemented"
+  (* Odoc_html.Generator.filepath ~config:html_config url *)
+
+  let extra_args = Term.const { Odoc_markdown.Config.root_url = None }
+  let renderer = { Odoc_document.Renderer.name = "markdown"; render; filepath }
+end)
+
 module Odoc_html_url : sig
   val cmd : unit Term.t
 
@@ -1772,6 +1785,7 @@ let () =
       Depends.Odoc_html.(cmd, info ~docs:section_deprecated);
       Classify.(cmd, info ~docs:section_pipeline);
       Extract_code.(cmd, info ~docs:section_pipeline);
+      Odoc_markdown_cmd.generate ~docs:section_pipeline;
     ]
   in
   let default =
