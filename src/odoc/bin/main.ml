737342bba931b1cc6e08a37e6c27d3e0adc88ed2
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-06-06 13:23:52 +0200

    Implement source_page in markdown

--- src/odoc/bin/main.ml
+++ src/odoc/bin/main.ml
@@ -1786,6 +1786,7 @@ let () =
       Classify.(cmd, info ~docs:section_pipeline);
       Extract_code.(cmd, info ~docs:section_pipeline);
       Odoc_markdown_cmd.generate ~docs:section_pipeline;
+      Odoc_markdown_cmd.generate_source ~docs:section_pipeline;
     ]
   in
   let default =
